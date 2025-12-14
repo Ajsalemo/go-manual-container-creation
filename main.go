297cf83fee6d6f391ebd19d4c9a3fd022dd8f6b1
Anthony Salemo <ajssalemo@gmail.com>
2025-12-13 20:44:32 -0500

    building out functionality to automatically download and create rootfs's

--- main.go
+++ main.go
@@ -3,14 +3,21 @@ package main
 import (
 	"crypto/rand"
 	"fmt"
+	"io"
 	"math/big"
+	"net/http"
 	"os"
 	"os/exec"
-	"strings"
+	"path/filepath"
 	"syscall"
 )
 
 func main() {
+	if err := createRootFS(); err != nil {
+		fmt.Println("Error creating root filesystem:", err)
+		os.Exit(1)
+	}
+
 	switch os.Args[1] {
 	case "run":
 		run()
@@ -66,16 +73,19 @@ func run() {
 	}
 }
 
-func child() {
-	fmt.Printf("Running %v as PID %d\n", os.Args[2:], os.Getpid())
-	// Set hostname generation to a length of 8
-	hostname, err := generateRandomString(8)
+func generateHostname() string {
+	host, err := generateRandomString(8)
 	if err != nil {
 		fmt.Println("Error generating random hostname:", err)
 		os.Exit(1)
 	}
-	//
-	fmt.Println("Generated random hostname:", hostname)
+	return host
+}
+
+var hostname = generateHostname()
+
+func child() {
+	fmt.Printf("Running %v as PID %d\n", os.Args[2:], os.Getpid())
 	// Set hostname of the new UTS namespace
 	// https://www.man7.org/linux/man-pages/man7/uts_namespaces.7.html - UTS namespace contains hostname and domain name
 	if err := syscall.Sethostname([]byte(hostname)); err != nil {
@@ -92,4 +102,44 @@ func child() {
 		fmt.Println("Error running the child command:", err)
 		os.Exit(1)
 	}
+}
+
+// create a new root file system for the container
+func createRootFS() error {
+	// 1. download a minimal filesystem from alpine
+	// 2. create a directory for the new root fs under /tmp/[hostname]/rootfs
+	// 3. extract the downloaded filesystem into the new root fs directory
+	// Create the file
+	url := "https://dl-cdn.alpinelinux.org/alpine/v3.18/releases/x86_64/alpine-minirootfs-3.18.0-x86_64.tar.gz"
+	filename := filepath.Base(url)
+	dir := "/tmp/" + hostname + "/rootfs"
+	// Build the full path in an OS-safe way
+	fullPath := filepath.Join(dir, filename)
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return fmt.Errorf("failed to create directory: %w", err)
+	}
+	// Create the file under /tmp/[hostname]/rootfs
+	out, err := os.Create(fullPath)
+	if err != nil {
+		return fmt.Errorf("failed to create file: %w", err)
+	}
+
+	defer out.Close()
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return fmt.Errorf("failed to download file: %w", err)
+	}
+	defer resp.Body.Close()
+	// Check server response
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("bad status: %s", resp.Status)
+	}
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return fmt.Errorf("failed to save file: %w", err)
+	}
+
+	return nil
 }
